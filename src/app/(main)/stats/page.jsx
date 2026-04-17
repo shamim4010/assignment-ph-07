6b2603578bd78bf77d65d1f8eac22c9f4edf8bad
shamim4010 <shamimislam401017@gmail.com>
2026-04-17 21:57:52 +0600

    feat: add PieChartStats component for visualizing friendship analytics and update package dependencies and project completed

--- src/app/(main)/stats/page.jsx
+++ src/app/(main)/stats/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+'use client'
+import PieChartStats from "../../../components/ui/othersUi/piechartstats/PieChartStats";
 
 function Stats() {
+  const timelineData = JSON.parse(localStorage.getItem('timelineData')) || [];
+
+  const filterCall = timelineData.filter(item => item[1] === 'Call');
+    const filterMessage = timelineData.filter(item => item[1] === 'Message');
+    const filterVideo = timelineData.filter(item => item[1] === 'Video');
   return (
-    <div>Stats</div>
-  )
+    <PieChartStats {...{timelineData, filterCall, filterMessage, filterVideo}} />
+  );
 }
 
 export default Stats
---
+++ src/components/ui/othersUi/piechartstats/PieChartStats.jsx
@@ -0,0 +1,54 @@
+"use client"
+import { Pie, PieChart } from 'recharts';
+import NoData from '../timeline/NoData';
+
+function PieChartStats({timelineData, filterCall, filterMessage, filterVideo}) {
+    const data = [
+        { name: 'Group A', value: filterCall.length, fill: '#244d3f' },
+        { name: 'Group B', value: filterMessage.length, fill: '#7f37f5' },
+        { name: 'Group C', value: filterVideo.length, fill: '#37a163' },
+    ];
+    return (
+        <div className='flex flex-col gap-4 m-24'>
+            <h2 className='text-4xl'>Friendship Analytics</h2>
+            <p className='text-[12px] ml-4'>By Interaction Type</p>
+            <div className='flex flex-col justify-center items-center gap-8 p-12'>
+                {timelineData.length === 0 ? <NoData /> : <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
+                    <Pie
+                        data={data}
+                        innerRadius="80%"
+                        outerRadius="100%"
+                        // Corner radius is the rounded edge of each pie slice
+                        cornerRadius="50%"
+                        fill="#8884d8"
+                        // padding angle is the gap between each pie slice
+                        paddingAngle={5}
+                        dataKey="value"
+                    />
+                </PieChart>}
+                <div>
+                    <div>
+                        <div aria-label="status" className="status bg-[#7f37f5] rounded-full"></div> 
+                        <span>
+                            Message
+                        </span>
+                    </div>
+                    <div>
+                        <div aria-label="status" className="status bg-[#244d3f] rounded-full"></div> 
+                        <span>
+                            Call
+                        </span>
+                    </div>
+                    <div>
+                        <div aria-label="status" className="status bg-[#37a163] rounded-full"></div> 
+                        <span>
+                            Video
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PieChartStats
